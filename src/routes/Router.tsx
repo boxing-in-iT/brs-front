1d5d2e853ac2c36701d71249835476f5a0f71622
boxing-in-iT <tbol240@gmail.com>
2025-04-11 13:37:56 +0300

    fix

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
 import { PATHNAMES } from "../constants/routes";
 import { HomePage } from "../pages/HomePage";
-import { AUTH_REFRESH_TOKEN } from "../constants/cookiesKeys";
-import Cookies from "js-cookie";
-import { isTokenExpired } from "../services/interceptors";
+// import { AUTH_REFRESH_TOKEN } from "../constants/cookiesKeys";
+// import Cookies from "js-cookie";
+// import { isTokenExpired } from "../services/interceptors";
 import { useLocation, useRoutes } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -16,7 +16,7 @@ const ROUTES = [
 ];
 
 const AppRoutes: FC = () => {
-  const refreshToken = Cookies.get(AUTH_REFRESH_TOKEN);
+  // const refreshToken = Cookies.get(AUTH_REFRESH_TOKEN);
   // const skip = !refreshToken || isTokenExpired(refreshToken);
 
   const location = useLocation();
